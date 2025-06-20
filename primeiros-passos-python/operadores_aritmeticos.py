6719c0deb41a082c7970e469da316870251436ac
Eduardo Berlatto <eduardoberlatto@gmail.com>
2025-06-20 13:50:28 -0300

    tipos de operadores com python

---
+++ primeiros-passos-python/operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+print("------------------")
+
+x = 10 + 5 * 4
+print(x)
+x = (10 + 5) * 4
+print(x)
+
+# Ordem de priorização: Parentesis, Expoentes, Multiplicações e Divisões(da esquerda para a direita)
+# Sempre deixar explícita a ordem desejada.
+y = (10 / 2) + (25 * 2) - (2 ** 2)
+print(y)
---
+++ primeiros-passos-python/operadores_associacao.py
@@ -0,0 +1,7 @@
+frutas = ["limao", "uva", "laranja", "mamão"]
+curso = "Curso de python"
+
+print("laranja" not in frutas)
+print("limao" in frutas)
+print("Python" in curso)
+print("mamão" in frutas)
